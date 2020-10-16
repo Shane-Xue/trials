65e2b7aea964d923dc1b34bce0a87c20b6a0ed88
Shane (Treasure) Xue <qq2198631773@outlook.com>
2020-10-16 13:21:15 +0800

    Create raii.cpp

---
+++ raii.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class b{
+public:
+  b(){
+    cout<<"in b constructor"<<endl;
+    }
+  ~b(){
+    cout<<"in b destructor"<<endl;
+    }
+  hey(){
+    cout<<"hey there!"<<endl;
+    }
+}
+
+int main(){
+  b::hey();
+  b boy;
+  boy.hey;
+  return 0;
+  }
